dd53138b8afbdeca7dad32c46bcf311eae1cd877
Priyanka Lakra <priyankalak06@gmail.com>
2025-11-05 19:58:06 +0530

    Add files via upload

---
+++ sql/superstore_sales_queries.sql
@@ -0,0 +1,30 @@
+use sales_db;
+
+CREATE TABLE ssales(
+    Row_ID INT,
+    Order_ID VARCHAR(50),
+    Order_Date DATE,
+    Ship_Date DATE,
+    Ship_Mode VARCHAR(50),
+    Customer_ID VARCHAR(50),
+    Customer_Name VARCHAR(100),
+    Segment VARCHAR(50),
+    Country VARCHAR(50),
+    City VARCHAR(100),
+    State VARCHAR(100),
+    Postal_Code VARCHAR(20),
+    Region VARCHAR(50),
+    Product_ID VARCHAR(50),
+    Category VARCHAR(50),
+    Sub_Category VARCHAR(50),
+    Product_Name VARCHAR(255),
+    Sales DECIMAL(10,2),
+    Quantity INT,
+    Discount DECIMAL(5,2),
+    Profit DECIMAL(10,2)
+);
+
+select * from ssales;
+
+
+
